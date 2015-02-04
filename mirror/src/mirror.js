eb405ea7be6bed67c00b64e0c49848729b03ed21
Adam Knight <adam@revolutionarysystems.co.uk>
2015-02-04 12:08:19 +0000

    Added basic record and playback functionality

--- mirror/src/mirror.js
+++ mirror/src/mirror.js
@@ -10,8 +10,10 @@ var PageMirror = function(options) {
 		socket = io();
 	}
 
+	var sessionId = window.location.hash.substring(1);
+
 	socket.emit("monitorSession", {
-		id: window.location.hash.substring(1)
+		id: sessionId
 	});
 
 	var mirror = new TreeMirror(document, {
@@ -31,16 +33,17 @@ var PageMirror = function(options) {
 		}
 	});
 
-	while (document.firstChild) {
-		document.removeChild(document.firstChild);
-	}
-
 	var initialized = false;
 	var connectionTimeoutId;
 
-	socket.on('initialize', function(args) {
+	var eventListeners = {};
+
+	eventListeners.initialize = function(args) {
 		window.clearTimeout(connectionTimeoutId);
-		if (initialized == false) {
+		if (initialized == false || args.new == true) {
+			while (document.firstChild) {
+				document.removeChild(document.firstChild);
+			}
 			try {
 				initialized = true;
 				base = args.base;
@@ -52,30 +55,90 @@ var PageMirror = function(options) {
 			} catch (e) {
 				console.error(e);
 			}
-		} else {
-			if (args.new == true) {
-				window.location.reload();
-			}
 		}
-	});
-	socket.on('applyChanged', function(args) {
+	};
+
+	eventListeners.applyChanged = function(args) {
 		mirror.applyChanged(args.removed, args.addedOrMoved, args.attributes, args.text);
-	});
-	socket.on('scroll', function(args) {
+	};
+
+	eventListeners.scroll = function(args) {
 		window.scrollTo(args.x, args.y);
-	});
-	socket.on('resize', function(args) {
+	}
+
+	eventListeners.resize = function(args) {
 		if (window.parent && window.parent.resize) {
 			window.parent.resize(args.width, args.height);
 		}
-	});
-	socket.on('unload', function(args) {
+	}
+
+	eventListeners.unload = function(args) {
 		var timeout = options.timeout || 10000;
-		var onTimeout = options.onTimeout || function(){
+		var onTimeout = options.onTimeout || function() {
 			document.body.innerHTML = "Connection timed out";
 		}
 		connectionTimeoutId = window.setTimeout(function() {
 			onTimeout(timeout);
 		}, timeout);
+	}
+
+	socket.on('initialize', function(args) {
+		eventListeners.initialize(args);
 	});
+	socket.on('applyChanged', function(args) {
+		eventListeners.applyChanged(args);
+	});
+	socket.on('scroll', function(args) {
+		eventListeners.scroll(args);
+	});
+	socket.on('resize', function(args) {
+		eventListeners.resize(args);
+	});
+	socket.on('unload', function(args) {
+		eventListeners.unload(args);
+	});
+
+	this.recordSession = function(){
+		socket.emit("recordSession", {
+			id: sessionId
+		});
+	}
+
+	this.stopRecordingSession = function(){
+		socket.emit("stopRecordingSession", {
+			id: sessionId
+		});
+	}
+
+	this.replaySession = function(id) {
+		var id = id || sessionId;
+		socket.emit("getRecordedSession", {
+			id: id
+		}, function(session) {
+			initialized = false;
+			applyEvents(session);
+		});
+	}
+
+	function applyEvents(events) {
+		var event = events.shift();
+		if (event) {
+			if (event.event == "wait") {
+				console.log("Waiting " + event.args.time + "ms");
+				setTimeout(function() {
+					applyEvents(events);
+				}, event.args.time);
+			} else {
+				eventListeners[event.event](event.args);
+				applyEvents(events);
+			}
+		}
+	}
+
+	function forEach(array, fn) {
+		for (var i in array) {
+			var item = array[i];
+			fn(item, i);
+		}
+	}
 }
--- server/src/server.js
+++ server/src/server.js
@@ -1,7 +1,7 @@
 // Load required modules
-var http    = require("http");              // http server core module
-var express = require("express");           // web framework external module
-var io      = require("socket.io");         // web socket external module
+var http = require("http"); // http server core module
+var express = require("express"); // web framework external module
+var io = require("socket.io"); // web socket external module
 
 // Setup and configure Express http server. Expect a subfolder called "static" to be the web root.
 var httpApp = express();
@@ -11,39 +11,100 @@ httpApp.use(express.static(__dirname + "/static/"));
 var webServer = http.createServer(httpApp).listen(8070);
 
 // Start Socket.io so it attaches itself to Express server
-var socketServer = io.listen(webServer, {"log level":1});
+var socketServer = io.listen(webServer, {
+  "log level": 1
+});
 
-socketServer.on('connection', function(socket){
+var recordingSessions = {};
+var recordedSessions = {};
+
+socketServer.on('connection', function(socket) {
+
   var sessionId;
+  var lastEventTime;
+
   console.log('a user connected');
-  socket.on('disconnect', function(){
+
+  socket.on('disconnect', function() {
     console.log('user disconnected');
   });
-  socket.on("createSession", function(args){
-  	console.log("Creating session " + args.id);
-  	sessionId = args.id;
-  	socket.join(sessionId);
-  	socketServer.to(sessionId).emit('monitoringSession');
+
+  socket.on("createSession", function(args) {
+    console.log("Creating session " + args.id);
+    sessionId = args.id;
+    socket.join(sessionId);
+    socketServer.to(sessionId).emit('monitoringSession');
   });
-  socket.on("monitorSession", function(args){
-  	console.log("Monitoring session " + args.id);
-  	sessionId = args.id;
-  	socket.join(sessionId);
-  	socket.broadcast.to(sessionId).emit('monitoringSession');
+
+  socket.on("recordSession", function(args) {
+    console.log("Recording session " + args.id);
+    sessionId = args.id;
+    recordingSessions[sessionId] = {startTime: new Date().getTime(), lastEventTime: new Date().getTime(), events: []};
+    socket.join(sessionId);
+    socket.broadcast.to(sessionId).emit('monitoringSession');
   })
-  socket.on('initialize', function(args){
+
+  socket.on("stopRecordingSession", function(args){
+    console.log("Stopped recording session " + args.id);
+    sessionId = args.id;
+    recordingSession = recordingSessions[sessionId];
+    recordingSession.endTime = new Date().getTime();
+    recordedSessions[sessionId] = recordingSession;
+    delete recordingSessions[sessionId];
+  });
+
+  socket.on("monitorSession", function(args) {
+    console.log("Monitoring session " + args.id);
+    sessionId = args.id;
+    socket.join(sessionId);
+    socket.broadcast.to(sessionId).emit('monitoringSession');
+  })
+
+  socket.on('initialize', function(args) {
+    recordEvent("initialize", args);
     socket.broadcast.to(sessionId).emit('initialize', args);
   });
-  socket.on('applyChanged', function(args){
+
+  socket.on('applyChanged', function(args) {
+    recordEvent("applyChanged", args);
     socket.broadcast.to(sessionId).emit('applyChanged', args);
   });
-  socket.on('scroll', function(args){
+
+  socket.on('scroll', function(args) {
+    recordEvent("scroll", args);
     socket.broadcast.to(sessionId).emit('scroll', args);
   });
-  socket.on('resize', function(args){
+
+  socket.on('resize', function(args) {
+    recordEvent("resize", args);
     socket.broadcast.to(sessionId).emit('resize', args);
   });
-  socket.on('unload', function(args){
+
+  socket.on('unload', function(args) {
+    recordEvent("unload", args);
     socket.broadcast.to(sessionId).emit('unload', args);
   });
+
+  socket.on('getRecordedSession', function(args, callback) {
+    var session = recordedSessions[args.id] || {events: []};
+    callback(session.events);
+  });
+
+  function recordEvent(event, args) {
+    var now = new Date().getTime();
+    var recordedSession = recordingSessions[sessionId];
+    if (recordedSession) {
+      recordedSession.events.push({
+        event: "wait",
+        args: {
+          time: now - recordedSession.lastEventTime
+        }
+      });
+      recordedSession.lastEventTime = now;
+      recordedSession.events.push({
+        event: event,
+        args: args
+      });
+    }
+  }
 });
